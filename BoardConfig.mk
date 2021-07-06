4b15d528b224dc87060069c7d3f200deedf74c7a
Harshit Jain <god@hyper-labs.tech>
2021-07-06 06:42:04 +0530

    x3: Enable HWUI_COMPILE_FOR_PERF
    * activates few optimizing compiler flags for HWUI

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
